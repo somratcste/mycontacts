349751d14e440df5db3992d36edeee1dedec7bb0
somratcste <somratcste@gmail.com>
2017-10-01 22:31:36 +0530

    list add

--- controllers/app.js
+++ controllers/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp",['ngRoute']);
+var myApp = angular.module("myApp",['ngRoute','firebase']);
 
 myApp.config(function ($routeProvider) {
     $routeProvider
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,5 @@
-myApp.controller('MainController' , function () {
-    
+myApp.controller('MainController' , function ($scope, $firebaseArray) {
+   var ref = new Firbase('https://mycontacts-2017.firebaseio.com/mycontacts-2017');
+   $scope.contacts = $firebaseArray(ref);
+   console.log($scope);
 });
