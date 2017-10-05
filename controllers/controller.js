044661f463ca5bbfadd6b4efc8ab241def2dab52
somratcste <somratcste@gmail.com>
2017-10-06 00:28:26 +0530

    connect firebase

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,4 @@
 myApp.controller('MainController' , function ($scope, $firebaseArray) {
-   var ref = new Firbase('https://mycontacts-2017.firebaseio.com/mycontacts-2017');
-   $scope.contacts = $firebaseArray(ref);
-   console.log($scope);
+    var rootRef = firebase.database().ref();
+   console.log(rootRef);
 });
